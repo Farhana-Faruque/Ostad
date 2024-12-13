8c1e16577b6082dd6df18211a46f759549072efe
Kazi Farhana Faruque <168679096+Farhana-Faruque@users.noreply.github.com>
2024-12-13 13:47:06 +0600

    Update Book.py

--- Project/Advance Library Management CLI/Book.py
+++ Project/Advance Library Management CLI/Book.py
@@ -3,6 +3,8 @@ import restore_all_books_file
 import add_books
 import view_all_books
 import delete_book
+import lend_books
+import return_books
 
 all_books = []
 
@@ -13,6 +15,8 @@ while True:
     print("2. View All Books")
     print("3. Book Update ")
     print("4. Book Remove/ Delete ")
+    print("5. Lend books ")
+    print("6. Return books ")
 
     all_books = restore_all_books_file.restore_all_books(all_books)
 
@@ -29,5 +33,9 @@ while True:
         update_all_books.update_books(all_books)
     elif menu == "4" :
         delete_book.delete_books(all_books)
+    elif menu == "5" :
+        lend_books.lend_books(all_books)
+    elif menu == "6" :
+        return_books.return_books(all_books)
     else :
         print("Choose a valid number.")
